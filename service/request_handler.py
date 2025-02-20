d7667936e053da7e3f1b42ebea7b4c4e37f68d26
work <galileosky.moshnyakov.v@gmail.com>
2025-02-20 12:34:28 +0500

    cleanup

--- service/request_handler.py
+++ service/request_handler.py
@@ -12,24 +12,26 @@ if not SECRET_KEY:
     raise ValueError("Ошибка: SECRET_KEY не найден в переменных окружения!")
 
 def handle_request(request):
-    # signature_header = request.headers.get('X-Huntflow-Signature')
-    # if not signature_header:
-    #     return jsonify({"error": "Отсутствует заголовок X-Huntflow-Signature"}), 401
-    #
-    # computed_signature = hmac.new(
-    #     key=SECRET_KEY.encode('utf-8'),
-    #     msg=request.data,
-    #     digestmod=hashlib.sha256
-    # ).hexdigest()
-    #
-    # if not hmac.compare_digest(computed_signature, signature_header):
-    #     return jsonify({"error": "Неверная подпись"}), 401
+    if request is None or request.get_json() is None:
+        return jsonify({"error": "Отсутствуют данные"}), 400
 
+    signature_header = request.headers.get('X-Huntflow-Signature')
+    if not signature_header:
+        return jsonify({"error": "Отсутствует заголовок X-Huntflow-Signature"}), 401
+
+    computed_signature = hmac.new(
+        key=SECRET_KEY.encode('utf-8'),
+        msg=request.data,
+        digestmod=hashlib.sha256
+    ).hexdigest()
+
+    if not hmac.compare_digest(computed_signature, signature_header):
+        return jsonify({"error": "Неверная подпись"}), 401
 
     event_type = request.headers.get('x-huntflow-event')
 
+    data = request.get_json()
     if event_type == 'APPLICANT':
-        data = request.get_json()
         handle_applicant(data)
 
-    return '', 200
+    return jsonify({"success": "Данные обработаны"}), 401
