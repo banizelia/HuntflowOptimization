3b8690d5a46a5c53c96d4c0626cc19f4c098bf92
work <galileosky.moshnyakov.v@gmail.com>
2025-02-28 13:37:58 +0500

    renamed variable

--- src/service/applicant_handler.py
+++ src/service/applicant_handler.py
@@ -28,25 +28,24 @@ def handle_applicant(data):
         logger.info("Статус кандидата: %s, не соответствует '%s'.", status_name, from_stage_name)
         return jsonify({"error": f"Статус кандидата: {status_name}, не соответствует '{from_stage_name}'."}), 400
 
-    applicant = event.get('applicant', {})
     candidate_id = applicant.get('id')
     vacancy_id = applicant_log.get('vacancy', {}).get('id')
 
-    if not candidate_id:
+    if not applicant_id:
         logger.error("ID кандидата не найден.")
         return jsonify({"error": f"ID кандидата не найден."}), 400
     if not vacancy_id:
         logger.error("ID вакансии не найден.")
         return jsonify({"error": f"ID вакансии не найден."}), 400
 
-    logger.info("Кандидат перешёл на этап '%s'. ID кандидата: %s, ID вакансии: %s",from_stage_name, candidate_id, vacancy_id)
-    candidate_evaluation_answer = evaluate_candidate(candidate_id, vacancy_id)
+    logger.info("Кандидат перешёл на этап '%s'. ID кандидата: %s, ID вакансии: %s",from_stage_name, applicant_id, vacancy_id)
+    candidate_evaluation_answer = evaluate_candidate(applicant_id, vacancy_id)
 
     target_stage_name = candidate_evaluation_answer.target_stage.value
     comment = candidate_evaluation_answer.comment
 
     target_stage_id = get_status_id_by_name(target_stage_name)
 
-    update_candidate_status(candidate_id, target_stage_id, vacancy_id, f"Оценка от ИИ: \n\n {comment}")
+    update_candidate_status(applicant_id, target_stage_id, vacancy_id, f"Оценка от ИИ: \n\n {comment}")
 
     return jsonify({"success": "Данные обработаны"}), 200
