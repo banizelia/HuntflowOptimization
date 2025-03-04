afd62781dd5a9b18d5b2adf2539f37e04d798739
work <galileosky.moshnyakov.v@gmail.com>
2025-03-04 18:19:24 +0500

    logging_config.py cleanup

--- src/config/logging_config.py
+++ src/config/logging_config.py
@@ -1,24 +1,24 @@
 import logging.config
 
-LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "format": "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+def setup_logging():
+    LOGGING_CONFIG = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+            },
         },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "default",
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+                "level": "INFO",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
             "level": "INFO",
         },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "INFO",
-    },
-}
-
-logging.config.dictConfig(LOGGING_CONFIG)
+    }
+    logging.config.dictConfig(LOGGING_CONFIG)
